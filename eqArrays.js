ed9605ad330290def47fd3bcfa18e5f3d21d012f
Jay Seo <sjs5953@hotmail.com>
2019-11-27 01:22:01 +0000

    created eqArray function

---
+++ eqArrays.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual,expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed ${actual} === ${expected}`);
+  }
+};
+
+let eqArrays = function (a,b) {
+  if (a.length===b.length) {
+    let result = true;
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {return false}
+    };
+    return result
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
