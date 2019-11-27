971a8bd694b8bf68161147000c21f6e82612c93d
Jay Seo <sjs5953@hotmail.com>
2019-11-27 02:00:15 +0000

    created assertioMiddle function

---
+++ middle.js
@@ -0,0 +1,41 @@
+let eqArrays = function(a,b) {
+  if (a.length === b.length) {
+    let result = true;
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return result;
+  }
+};
+
+let assertArraysEqual = function(q,p) {
+  if (eqArrays(q,p) === true) {
+    console.log(`Assertion Passed: ${q} === ${p}`);
+  } else if (eqArrays(q,p) === false) {
+    console.log(`Assertion Failed: ${q} !== ${p}`);
+  }
+};
+
+let middle = function(arr) {
+  const midIndex = [];
+  if (arr.length % 2 !== 0) {
+    midIndex.push(Math.floor(arr.length / 2));
+  } else {
+    midIndex.push(Math.floor(arr.length / 2) - 1);
+    midIndex.push(Math.floor(arr.length / 2));
+  }
+  let result = [];
+  for (let w = 0; w < midIndex.length; w ++) {
+    result.push(arr[midIndex[w]]);
+  }
+  return result;
+};
+
+
+let assertMiddle = function(inputArry,outputArray) {
+  assertArraysEqual(middle(inputArry),outputArray);
+};
+
+assertMiddle([1,2,3,4],[2,3]);
