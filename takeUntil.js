18cbf61fe33b8431b20b01934c946b3286c7ed72
Jay Seo <sjs5953@hotmail.com>
2019-11-29 06:56:16 +0000

    test

--- takeUntil.js
+++ takeUntil.js
@@ -42,4 +42,4 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 // console.log(results2);
-assertArraysEqual(results2,[ 'I\'ve', 'been', 'to', 'Hollywood' ])
+assertArraysEqual(results2,['I\'ve', 'been', 'to', 'Hollywood'])
