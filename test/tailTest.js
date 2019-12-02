e167784d04e8158666502b62fbeac77d1524ed0e
Jay Seo <sjs5953@hotmail.com>
2019-12-02 23:28:03 +0000

    added

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,11 +2,9 @@ const chai = require('chai');
 const tail = require('../tail');
 const assert = chai.assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
 
 describe("#tail", () => {
   it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
     assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
   });
 });
-
